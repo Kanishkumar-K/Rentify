ec4aa0fa5ae176b53f8891154992caeb611d9565
Kanishkumar <kanishkumar.24cs@licet.ac.in>
2024-05-27 18:37:35 +0530

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,19 +3,18 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet
-from . import views
-
 
 router = DefaultRouter()
 router.register(r'properties', PropertyViewSet)
+
 urlpatterns = [
     path('', views.home, name='home'),  # Define a URL pattern for the home page
     path('token/', views.MyTokenObtainPairView.as_view(), name="token-obtain"),
     path('token/refresh/', TokenRefreshView.as_view(), name="refresh-token"),
     path('register/', views.RegisterView.as_view(), name="register-user"),
     path('test/', views.protectedView, name="test"),
-    path('dashboard/', views.dashboard, name="dashboard"),  #logged in
-    path('', views.view_all_routes, name="all-routes"),
+    path('dashboard/', views.dashboard, name="dashboard"),  # Logged in
     path('properties/<int:property_id>/', views.edit_delete_property, name="edit-delete-property"),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),  # Include router URLs under 'api/' prefix
+    path('all-routes/', views.view_all_routes, name="all-routes"),  # Moved this pattern to a different URL
 ]
